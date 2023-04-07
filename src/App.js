6e2d91a3c85a5460fd710c9ce0ace3ecb49c9460
kavi <sumathi291098@gmail.com>
2023-04-07 17:56:13 +0530

    Controlled components

--- src/App.js
+++ src/App.js
@@ -4,28 +4,38 @@ import './App.css';
 function App() {
   const [newName,setName] = useState('');
   const [newAge,setAge] = useState('');
+  const [clgName,setClgName] = useState('');
   const [items,setItems] = useState([]);
   const displayName = (event) => {
     setName(event.target.value);
   }
   const displayAge = (event) => {
-    if(event.target.value <= 0){
-      alert('Please enter the proper age of a person!!');
-    }
+    
     setAge(event.target.value);
   }
+  const displayClgName = (event) => {
+    setClgName(event.target.value);
+  }
   const displayValue = () => {
-    if(!newName && !newAge){
-      alert('Please enter the details!!');
-    }
+    
     const item = {
       id: Math.floor(Math.random()),
       name: newName,
-      age: newAge
+      age: newAge,
+      clgname: clgName
+    }
+    if(newName.trim().length === 0 || newAge.trim().length === 0 || clgName.trim().length === 0){
+      alert('Please enter the details before submitting');
+      return;
+    }
+    if(+newAge < 1){
+      alert('Entered age is invalid');
+      return;
     }
     setItems(oldList => [...oldList,item]);
     setName('');
     setAge('');
+    setClgName('');
   }
 
   return (
@@ -39,6 +49,9 @@ function App() {
       <input type='number' placeholder='Enter your age..' onChange={displayAge} value={newAge}></input>
       </div>
       <div>
+      <input type='text' placeholder='Enter your College name..' onChange={displayClgName} value={clgName}></input>
+      </div>
+      <div>
         <button onClick={displayValue}>Add user</button>
       </div>
       </div>
@@ -47,7 +60,7 @@ function App() {
           {
             items.map((ele) => {
               return(
-                <li id={ele.id}>{ele.name} ({ele.age} years old)</li>
+                <li id={ele.id}>{ele.name} ({ele.age} years old) {ele.clgname}</li>
               )
             })
           }
